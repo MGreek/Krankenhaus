cc8dbaa8370dda9ad51d1f3b0832fb8c927ca366
MGreek <marcgrec2003@gmail.com>
2022-10-11 11:39:55 +0300

    Implemented parts of Controller, View and Person

--- Controller.java
+++ Controller.java
@@ -1,2 +1,48 @@
+import java.util.*
 public class Controller {
+    private Integer globalId;
+
+    private Vector<Person> persons;
+
+    public void create()
+    {
+        globalId = 0;
+        persons = new Vector<Person>();
+    }
+
+    public Vector<Person> getPersons()
+    {
+        return persons;
+    }
+
+    public int addPerson(String name)
+    {
+        Person person = new Person(++globalId, name);
+        persons.add(person);
+        return person.getId();
+    }
+
+    public boolean removePerson(Integer id)
+    {
+        for (int i = 0; i < persons.length; ++i)
+        {
+            if (persons[i].getId() == id)
+            {
+                persons.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean updatePerson(Integer id, Person newPerson)
+    {
+        for (int i = 0; i < persons.length; ++i) {
+            if (persons[i].getId() == id) {
+                persons[i] = newPerson;
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- View.java
+++ View.java
@@ -1,2 +1,18 @@
+import java.util.*
+
 public class View {
+    private Controller controller;
+
+    public void setController(Controller newController)
+    {
+        controller = newController;
+    }
+
+    public void printPersons()
+    {
+        for (int i = 0; i < controller.getPersons().length; ++i)
+        {
+            System.out.println(controller.getPersons()[i].getName());
+        }
+    }
 }
